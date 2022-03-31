d13d8b5e0fb08a76bfbbbb245ffb97d72b91034f
MyNameIsClown <vc.business.oo@gmail.com>
2022-03-31 18:46:16 +0200

    Modificacion usando GitHub Desktop

--- src/Principal.java
+++ src/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("Hola soy Victor");
-		System.out.println("Esto es una edicion del codigo desde GitHub")
+		System.out.println("Esto es una edicion del codigo desde GitHub");
+		System.out.println("Esto es otra modificacion");
 	}
 }
